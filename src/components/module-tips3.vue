e0e07036c3e98a7a853f2028f09818448b5bea61
rankaihua <rankaihua@aa.com>
2022-09-29 11:36:53 +0800

    1
    1

--- src/components/module-tips3.vue
+++ src/components/module-tips3.vue
@@ -2,7 +2,7 @@
   <div class="module-tips">
     <div class="module">
       <header>策略原理讲解及选股技巧</header>
-      <p>主力操盘运作流程主要有四个：建仓、洗盘、拉升、出货。但在拉主升浪之前，主力往往会拉上引线试盘，试探对手盘的抛压。如果抛压小就开启主升浪行情，也是我们买入的最好时机。</p>
+      <p>主力操盘运作流程主要有四个：建仓、洗盘、拉升、出货。但在拉主升浪之前，主力往往会拉上影线试盘，试探对手盘的抛压。如果抛压小就开启主升浪行情，也是我们买入的最好时机。</p>
       <img class="pick" :src="require('@/assets/img/web/pick1@2x.png')" />
       <div class="attrs">
         <div>
