af358b84ab43d89572768fe6e75e7f6874cd1058
yurongshanyu@163.com <71Q283j495K610>
2022-10-13 09:34:40 +0800

    修改舞阳

--- src/config/typeConfig.js
+++ src/config/typeConfig.js
@@ -4,7 +4,7 @@ const minuteLargeDdePulseQulet = {
   titleImg: 'title1@2x.png',
   tipsTop: '盘中实时(9:30-15:00)更新近日曾经超大单净量拉涨停或者长阳线的个股回调机会。最好结合热点选股。详细请看视频教程和案例。',
   titleWebImg: 'titleWeb1@2x.png',
-  price: 1688,
+  price: 1288,
 };
 
 const minutePulseQulet = {
@@ -13,7 +13,7 @@ const minutePulseQulet = {
   titleImg: 'title2@2x.png',
   tipsTop: '盘中实时（9:30-15:00）更新近日曾经大资金流入并且出现大阳线的个股回调机会。最好结合热点选股。详细请看视频教程和案例。',
   titleWebImg: 'titleWeb2@2x.png',
-  price: 1588,
+  price: 588,
 };
 
 const minuteUpShadow = {
@@ -22,7 +22,7 @@ const minuteUpShadow = {
   titleImg: 'title3@2x.png',
   tipsTop: '盘中实时（13:00-15:00）更新底部大幅拉升试盘或长时间封板尾盘被动开板的股票。详细请看视频教程和案例。',
   titleWebImg: 'titleWeb3@2x.png',
-  price: 1588,
+  price: 588,
 };
 
 const minuteDivingGold = {
@@ -31,7 +31,7 @@ const minuteDivingGold = {
   titleImg: 'title4@2x.png',
   tipsTop: '盘中实时（9:30-15:00）更新股价持续下跌，但是主力大资金持续买入的股票。详细请看视频教程和案例。',
   titleWebImg: 'titleWeb4@2x.png',
-  price: 1388,
+  price: 388,
 };
 
 module.exports = {
