42e446a063387d8a431e687a541a73fbd11eadb5
liming <ming.li@mail.dealmoon.com>
2023-01-16 11:54:24 +0800

    fix: bug

--- src/App.vue
+++ src/App.vue
@@ -64,6 +64,7 @@ export default {
       }, 2000);
       let that = this;
       window.appValidate = function (appGlobal) {
+        console.log(appGlobal);
         appGlobal = JSON.parse(appGlobal);
         console.log('set app global', appGlobal);
         that.setAppGlobal(appGlobal);
