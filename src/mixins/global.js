629522ce01c4af38495b5d6c731d7c8668341438
liming <ming.li@mail.dealmoon.com>
2023-01-16 13:15:15 +0800

    build

--- src/mixins/global.js
+++ src/mixins/global.js
@@ -109,6 +109,7 @@ export default {
         console.log('start method:', `${name}_${this.appGlobal.appName}`);
         window.wjs[`${name}_${this.appGlobal.appName}`].apply(this, JSON.stringify(params));
       } catch (error) {
+        console.error(error);
         console.log('no such method:', `${name}_${this.appGlobal.appName}`);
       }
     },
