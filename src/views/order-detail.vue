6ae20cb7a6c4f22c789390400dba643c3fab2774
liming <ming.li@mail.dealmoon.com>
2023-02-06 15:41:48 +0800

    build

--- src/views/order-detail.vue
+++ src/views/order-detail.vue
@@ -43,7 +43,7 @@
         <span>Account No.</span>
         <span>{{ detail.bankCardNo }}</span>
       </div>
-      <div class="flex-between" v-if="detail.orderBillId">
+      <div class="flex-between">
         <span>Loan ID</span>
         <span>{{ detail.orderNo }}</span>
       </div>
