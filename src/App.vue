e4ed725745fb6c121932d857d54f17349547c60b
liming <ming.li@mail.dealmoon.com>
2023-02-28 20:09:03 +0800

    build

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@ export default {
   mounted() {},
   watch: {
     $route(to, from) {
+      
       document.body.style.overflow = '';
       document.title = to.meta.title || '';
       this.toAppMethod('needBackControl', {need: false});
