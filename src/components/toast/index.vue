ff176371a8c6ef385acaa5f5d246e5046bf5a106
liming <ming.li@mail.dealmoon.com>
2023-03-23 15:30:44 +0800

    build

--- src/components/toast/index.vue
+++ src/components/toast/index.vue
@@ -1,24 +1,34 @@
 <template>
   <div class="toast" v-if="show">
-    {{ msg }}
+    <div class="toast-center">
+      {{ msg }}
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .toast {
   position: fixed;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  border-radius: 3px;
-  max-width: 200px;
-  padding: 10px;
-  background: #333;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: transparent;
   color: #fff;
   font-size: 14px;
   opacity: 0.9;
   text-align: justify;
   word-break: break-all;
   word-wrap: break-word;
+  &-center {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 3px;
+    max-width: 200px;
+    padding: 10px;
+    background: #333;
+  }
 }
 </style>
