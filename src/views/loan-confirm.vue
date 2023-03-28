8bc46dc5f212ab37daa4b5ae1c23eefe6d998f97
liming <ming.li@mail.dealmoon.com>
2023-03-28 14:47:18 +0800

    build

--- src/views/loan-confirm.vue
+++ src/views/loan-confirm.vue
@@ -82,7 +82,7 @@ export default {
         this.eventTracker('confirm_submit');
         await this.$http.post(`/zihai/bmzcx`, { orderId: this.orderId });
         // 成功或者失败的跳转
-        this.innerJump('loan-success-multi', { orderId: this.orderId, single: true, curNumbers: 1, systemTime: new Date().getTime() }, true);
+        this.innerJump('loan-success-multi', { orderId: this.orderId, single: true, systemTime: new Date().getTime() }, true);
       } catch (error) {
         this.$toast(error.message);
         setTimeout(() => {
