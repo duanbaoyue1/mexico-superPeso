60a732afd080936828ad12dcedc104300c31454d
liming <ming.li@mail.dealmoon.com>
2023-04-04 22:24:16 +0800

    fix: 调试bridge

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -9,18 +9,32 @@
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex';
+
 export default {
   data() {
     return {
       query: this.$route.query,
+      from: this.$route.query.from,
       loading: false,
       count: 1,
     };
   },
+  created() {
+    this.setGlobalData();
+  },
   methods: {
+    ...mapActions(['setAppGlobal', 'setAppChecked', 'updateToken']),
+
+    setGlobalData() {
+      if (this.from == 'bridge') {
+        console.log(this.query);
+        this.setAppGlobal(this.query);
+      }
+      this.setAppChecked(true);
+    },
     onRefresh() {
       console.log('onRefresh');
-
       setTimeout(() => {
         this.$toast('刷新成功');
         this.loading = false;
