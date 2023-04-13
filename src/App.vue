62b9471f16e5fb89481a6c1f71311b325c3f7ad7
liming <ming.li@mail.dealmoon.com>
2023-04-13 18:18:54 +0800

    fix: bug

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
       <nav-bar v-if="tabBar.show" />
       <transition name="fade">
         <keep-alive v-if="$route.meta.keepAlive">
-          <div>{{ $route.meta.keepAlive }}</div>
           <!-- 这里是会被缓存的视图组件 -->
           <router-view id="view" />
         </keep-alive>
